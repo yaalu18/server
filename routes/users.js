3cbebfad959198245cbf4e97b6299fd3927bd689
unknown <yaalu18@gmail.com>
2024-06-21 09:33:51 +0530

    app crash solve 3

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ import auth from "../middlewares/auth.js";
 
 const router=express.Router()
 router.post('/signup',signup)
-router.post('/login',login)
+//router.post('/login',login)
 router.get('/getAllUsers', getAllUsers);
 //router.patch('/update/:id', auth, updateProfile);
 export default router
