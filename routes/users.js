a698bef91fb400806c9a57f1df454f83937bf581
unknown <yaalu18@gmail.com>
2024-06-21 09:26:52 +0530

    app crash solve 2

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 import express  from "express";
-import {login,signup} from '../controllers/Auth.js'
+import {signup} from '../controllers/Auth.js'
 import { getAllUsers } from "../controllers/users.js";
 //import { updateProfile } from "../controllers/users.js";
 import auth from "../middlewares/auth.js";
