1578034af636e5b5b32eabbe254a2a0dce7b5d4d
unknown <yaalu18@gmail.com>
2024-06-26 14:05:07 +0530

    changing askquestion1

--- controllers/Question.js
+++ controllers/Question.js
@@ -3,6 +3,7 @@ export const AskQuestion = async (req, res) => {
     const postQuestionData = req.body;
     const postQuestion = new Question({ ...postQuestionData, userId: req.userId });
     try {
+        console.log("I am here inside Askquestion component")
         await postQuestion.save();
         res.status(200).json("Posted a question successfully");
     } catch (error) {
