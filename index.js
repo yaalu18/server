6e185b8c4f3d18bded4cf0d270de2bef7b312443
unknown <yaalu18@gmail.com>
2024-06-26 13:09:46 +0530

    question section 6

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import userRoutes from './routes/users.js';
-import {Router} from './routes/Question.js';
+import router from './routes/Question.js';
 
 dotenv.config();
 
@@ -31,7 +31,7 @@ app.get('/check', (req, res) => {
 });
 
 app.use('/user', userRoutes);
-app.use('/questions', Router);
+app.use('/questions', router);
 
 const PORT = process.env.PORT || 5000;
 const CONNECTION_URL = "mongodb+srv://yaalu18:admin1@stackoverflow-backend-d.ecqpkpw.mongodb.net/";
--- routes/Question.js
+++ routes/Question.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { AskQuestion } from "../controllers/Question.js";
+import auth from "../middlewares/auth.js";
 const router=express.Router()
 router.post('/Ask',AskQuestion)
 export default router
