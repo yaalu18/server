f4a4f66bc61c81229be1cf1d1fb889f01c4a0901
unknown <yaalu18@gmail.com>
2024-06-26 12:21:58 +0530

    question section 5

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import userRoutes from './routes/users.js';
-import {questionRoutes} from './routes/Question.js';
+import {Router} from './routes/Question.js';
 
 dotenv.config();
 
@@ -31,7 +31,7 @@ app.get('/check', (req, res) => {
 });
 
 app.use('/user', userRoutes);
-app.use('/questions', questionRoutes);
+app.use('/questions', Router);
 
 const PORT = process.env.PORT || 5000;
 const CONNECTION_URL = "mongodb+srv://yaalu18:admin1@stackoverflow-backend-d.ecqpkpw.mongodb.net/";
