e7e75fc284e8bd063ff6f9332c1d73ecd7ca7efd
yaalu18 <146164534+yaalu18@users.noreply.github.com>
2024-06-27 16:31:35 +0530

    update tag as array of string

--- models/Questions.js
+++ models/Questions.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 const QuestionSchema=mongoose.Schema({
     questionTitle:{type:String,required:"Question must have a title"},
     questionBody:{type:String,required:"Question must have a body"},
-    questionTags:{type:String,required:"Question must have a body"},
+    questionTags:{type:[String],required:"Question must have a body"},
     noOfAnswers:{type:Number,default:0},
     upVote:{type:[String],default:[]},
     downVote:{type:[String],default:[]},
